912dd5684b548e132f6933d0f64f150b6cf5ce60
Alikhan Batchaev <loko-han@mail.ru>
2020-01-12 20:12:00 +0300

    Initial Commit

---
+++ Magic 8 ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 ball
+//
+//  Created by 🧔🏻Alikhan Batchaev on 12.01.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
